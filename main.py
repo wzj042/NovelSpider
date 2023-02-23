0cd1f6594c06fed691905b885b5f1ac73b98cce8
Xqh233 <1575157146@qq.com>
2023-02-24 00:09:16 +0800

    修改了gitignore

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from core.diyibanzhu import diyibanzhu
 
 
 if __name__ == '__main__':
-    """每次加载时设置baseurl"""
     main = diyibanzhu()
     # chapter = main.get_chpater('http://www.2diyibanzhu.cc/32/32880/754225_4.html')
+    # main.save_chapter(chapter = chapter)
+
+    """其实可以更精简一点是吧"""
     novel = main.get_novel('http://www.2diyibanzhu.cc/23/23044/')
     main.save_novel(novel)
-
-    
 
     
     
